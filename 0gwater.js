e36b92e9f93cb8e160a4f7074c5a02f087ac0558
aaabbbccc <jamesopjworker@gmail.com>
2024-04-20 14:11:12 +0800

    update

--- 0gwater.js
+++ 0gwater.js
@@ -6,11 +6,22 @@ main();
 
 async function main() {
 
+    var address;
 
     exec('evmosd keys add wallet --dry-run',
         function (error, stdout, stderr) {
          var a = stdout.split(" ");
+         address = a[2];
          console.log(a);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+
+    exec('evmosd debug addr ' + address,
+        function (error, stdout, stderr) {
+
+            console.log(stdout);
             if (error !== null) {
                 console.log('exec error: ' + error);
             }
