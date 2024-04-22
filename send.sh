6328a1d12af3c1169ebd7731dd0ea29422beab70
aaabbbccc <jamesopjworker@gmail.com>
2024-04-22 23:31:16 +0800

    update

--- send.sh
+++ send.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/expect
-evmosd tx bank send wallet $1 1000000aevmos --gas=1 --gas-prices=1aevmos -y
+set timeout 30
+spawn evmosd tx bank send wallet $1 1000000aevmos --gas=1 --gas-prices=1aevmos -y
 expect "*passphrase*"
 send "11111111\r"
