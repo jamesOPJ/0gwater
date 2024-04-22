9e3016c7d11ffba59128031e114a8943a88b748f
aaabbbccc <jamesopjworker@gmail.com>
2024-04-22 23:22:01 +0800

    update

--- send.sh
+++ send.sh
@@ -1,1 +1,3 @@
-evmosd tx bank send wallet $1 1000000aevmos --gas=1 --gas-prices=1aevmos -y
+spawn evmosd tx bank send wallet $1 1000000aevmos --gas=1 --gas-prices=1aevmos -y
+expect "*passphrase*"
+send "11111111\r"
