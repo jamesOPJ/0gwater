b841d5ff47e7c3fe9c0a755004c603f554778506
aaabbbccc <jamesopjworker@gmail.com>
2024-04-23 00:16:51 +0800

    update

---
+++ balance.sh
@@ -0,0 +1,3 @@
+evmosd q bank balances $(evmosd keys show wallet -a) << EOF
+11111111
+EOF
--- send.sh
+++ send.sh
@@ -1,7 +1,6 @@
-expect <<EOF
-spawn evmosd tx bank send wallet $1 1000000aevmos --gas=500000 --gas-prices=99999aevmos -y
-expect "passphrase"
-send "11111111\r"
+evmosd tx bank send wallet $1 1000000aevmos --gas=500000 --gas-prices=99999aevmos -y << EOF
+11111111
+y
 EOF
 
 
