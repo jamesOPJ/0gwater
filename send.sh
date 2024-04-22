0b4820c5bf2bd67f1bb76642107e7a92e090b72d
aaabbbccc <jamesopjworker@gmail.com>
2024-04-22 23:38:56 +0800

    update

--- send.sh
+++ send.sh
@@ -1,6 +1,6 @@
 #!~/usr/bin/expect
 #set timeout 30
-#spawn evmosd tx bank send wallet $1 1000000aevmos --gas=1 --gas-prices=1aevmos -y
-#expect "*passphrase*"
-#send "11111111\r"
-expect -c ‘spawn evmosd tx bank send wallet $1 1000000aevmos --gas=1 --gas-prices=1aevmos -y ; expect passphrase ; send “11111111\n” interact’
+spawn evmosd tx bank send wallet $1 1000000aevmos --gas=1 --gas-prices=1aevmos -y
+expect "*passphrase*"
+send "11111111\r"
+#expect -c ‘spawn evmosd tx bank send wallet $1 1000000aevmos --gas=1 --gas-prices=1aevmos -y ; expect passphrase ; send “11111111\n” interact’
